10f7669fb05e5610aef400c3c49c4067b9dc9862
Andrey <AndreyBurk@yandex.ru>
2023-06-05 19:02:49 +0300

    6_Sprint

---
+++ src/test/java/LionParametersTest.java
@@ -0,0 +1,41 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(Parameterized.class)
+public class LionParametersTest {
+
+    String sex;
+    boolean hasMane;
+    Feline feline = Mockito.mock(Feline.class);
+
+    public LionParametersTest(String sex, boolean hasMane) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] lionSexParameters() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void doesHaveManeReturnActualHasMane() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        Assert.assertEquals(hasMane, lion.doesHaveMane());
+    }
+}
